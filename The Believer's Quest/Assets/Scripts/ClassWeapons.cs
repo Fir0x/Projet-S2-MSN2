c95b197a428ffd6aacb2333ad3bba683d6dcc1ae
Nicolas INDJEIN <nicolas.indjein@epita.fr>
2019-01-21 22:49:23 +0100

    transfert

---
+++ The Believer's Quest/Assets/Scripts/ClassWeapons.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    protected int damage;
+    protected float cooldown;
+    protected float reloadingTime;
+    protected int loader;
+    protected readonly int loaderCappacity;
+    protected readonly int maxAmmunitions;
+    protected int ammunitions;
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public int GetAmmunitions()
+    {
+        return ammunitions;
+    }
+
+    public void SetAmmunitions(int ammunitions)
+    {
+        this.ammunitions = ammunitions;
+    }
+
+    public void AddAmmunitions(int location, int numAmmo, int maximum)
+    {
+        location += numAmmo;
+        if (location > maximum)
+            location -= location - maximum;
+    }
+
+    IEnumerator ReloadTimer()
+    {
+        yield return new WaitForSeconds(reloadingTime);
+    }
+
+    public void Reload()
+    {
+        AddAmmunitions(loader, ammunitions, loaderCappacity);
+        print("Reload starts"); //DEBUG LOG
+        StartCoroutine(ReloadTimer());
+        print("Reload ends"); //DEBUG LOG
+    }
+}
