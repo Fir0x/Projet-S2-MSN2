1c01611a47afa9821088af82a008b87b56d54ae7
Nicolas INDJEIN <nicolas.indjein@epita.fr>
2019-02-27 20:53:45 +0100

    génération de salle presque terminée => murs à coller et bug porte à régler

---
+++ The_Believer's_Quest/Assets/Scripts/Board.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width = 10;
+    public int height = 5;
+    public int roomWidth = 9;
+    public int roomHeight = 6;
+    public int roomNumber = 3;
+    private bool[,] roomsPosition;
+    private Room[,] roomsArray;
+    private Room room;
+    private Transform board;
+
+    public void BoardCreation()
+    {
+        board = new GameObject("Board").transform;
+        roomsPosition = new bool[height, width];
+        roomsArray = new Room[height, width];
+        List<int[]> usedPosition = new List<int[]>();
+        //int[] lastPosition = new int[] { Random.Range(0, width - 1), Random.Range(0, height - 1) };
+        int[] startPoint = new int[] { 0, 0 };
+        roomsPosition[startPoint[1], startPoint[0]] = true;
+        usedPosition.Add(startPoint);
+        int lastX;
+        int lastY;
+
+        for (int i = 1; i < roomNumber; i++)
+        {
+            int[] newPosition = new int[2];
+            lastX = startPoint[0];
+            lastY = startPoint[1];
+            //print("LastX = " + lastX + "\nLastY = " + lastY); //DEBUG
+
+            do
+            {
+                newPosition[0] = Random.Range(lastX - 1, lastX + 2);
+                if (newPosition[0] == lastX)
+                    newPosition[1] = Random.Range(lastY - 1, lastY + 2);
+            } while (newPosition == startPoint || newPosition[0] < 0 || newPosition[0] >= width ||
+                    newPosition[1] < 0 || newPosition[1] >= height || 
+                    usedPosition.Exists(pos => pos[0] == newPosition[0] && pos[1] == newPosition[1]));
+
+            //print("newX = " + newPosition[0] + "\nnewY = " + newPosition[1]); //DEBUG
+            roomsPosition[newPosition[1], newPosition[0]] = true;
+            usedPosition.Add(newPosition);
+            startPoint = usedPosition[Random.Range(0, usedPosition.Count)];
+        }
+        print(usedPosition.Count); //DEBUG
+
+        List<int[]> doorsPosition = new List<int[]>();
+        int roomN = 1;
+        float[] anchor = new float[2];
+
+        string visual = "";
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                visual += roomsPosition[i, j];
+            }
+            visual += '\n';
+        }
+        print(visual); //DEBUG
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (roomsPosition[i,j])
+                {
+                    anchor[0] = j * roomWidth * 0.32f;
+                    anchor[1] = i * roomHeight * 0.32f;
+                    if (j - 1 >= 0 && roomsPosition[i, j - 1])
+                        doorsPosition.Add(new int[] { 0, (roomHeight - 1) / 2 });
+
+                    if (j + 1 < width && roomsPosition[i, j + 1])
+                        doorsPosition.Add(new int[] { width - 1, (roomHeight - 1) / 2 });
+
+                    if (i - 1 >= 0 && roomsPosition[i - 1, j])
+                        doorsPosition.Add(new int[] { (roomWidth - 1) / 2, 0 });
+
+                    if (i + 1 < height && roomsPosition[i + 1, j])
+                        doorsPosition.Add(new int[] { (roomWidth - 1) / 2, height - 1 });
+
+                    room = GetComponent<Room>();
+                    room.RoomCreator(roomWidth, roomHeight, anchor, roomN, doorsPosition);
+                    room.transform.SetParent(board);
+                    roomsArray[i, j] = room;
+                    doorsPosition.Clear();
+                    roomN += 1;
+                }
+            }
+        }
+    }
+
+    public void PrintBoard()
+    {
+        foreach (Room room in roomsArray)
+            room.RoomSetup();
+    }
+
+    public void Start()
+    {
+        BoardCreation();
+    }
+}
--- The_Believer's_Quest/Assets/Scripts/Room.cs
+++ The_Believer's_Quest/Assets/Scripts/Room.cs
@@ -7,36 +7,38 @@ public class Room : MonoBehaviour
 {
     private int width;
     private int height;
-    private int[] anchor;
+    private float[] anchor;
     private int roomNumber;
     private List<int[]> doorsPosition;
     public GameObject[] floorTiles;
     public GameObject[] wallTiles;
     private GameObject[,] roomArray;
-    private Transform board;
+    private Transform room;
 
-    public Room(int width, int height, int[] anchor, int roomNumber, List<int[]> doorsPosition)
+    public void RoomCreator(int width, int height, float[] anchor, int roomNumber, List<int[]> doorsPosition)
     {
         this.width = width;
         this.height = height;
         this.anchor = anchor;
         this.roomNumber = roomNumber;
         this.doorsPosition = doorsPosition;
+
         roomArray = new GameObject[height, width];
-        board = new GameObject("Room " + roomNumber).transform;
+        room = new GameObject("Room " + roomNumber).transform;
         GameObject tile;
         GameObject instance;
-        int[] position = new int[2];
 
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                position[0] = j;
-                position[1] = i;
-                tile = (i == 0 || i == height - 1 || j == 0 || j == width - 1 || !doorsPosition.Contains(position)) ? wallTiles[0] : floorTiles[0];
-                instance = Instantiate(tile, new Vector2(anchor[0] + (float)j / 3.125f, anchor[1] + (float)i / 3.125f), Quaternion.identity) as GameObject;
-                instance.transform.SetParent(board);
+                tile = ((i == 0 || i == height - 1 || j == 0 || j == width - 1) &&
+                    !doorsPosition.Exists(pos => pos[0] == j && pos[1] == i)) ? wallTiles[0] : floorTiles[0];
+
+                instance = Instantiate(tile, new Vector2(anchor[0] + (float)j / 3.125f, anchor[1] + (float)i / 3.125f), 
+                    Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(room);
                 roomArray[i, j] = instance;
             }
         }
