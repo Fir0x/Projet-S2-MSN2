2b91ff55672942ef9d7fc8f0fa9aba3686facd71
Unikarah <sarah.gutierez@epita.fr>
2019-04-02 18:51:06 +0200

    menu principal basique terminé

---
+++ The_Believer's_Quest/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void START_GAME()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    public void QUIT_GAME()
+    {
+       Application.Quit(); 
+    }
+
+}
