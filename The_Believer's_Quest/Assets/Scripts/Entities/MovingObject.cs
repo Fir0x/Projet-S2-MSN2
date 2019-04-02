2ba97072ac9bfb39d6276726bd972a4e3b3fc3f2
TheNikow <nicolas.lorrain@epita.fr>
2019-04-02 19:57:24 +0200

    problème collisions normalement réglé

--- The_Believer's_Quest/Assets/Scripts/Entities/MovingObject.cs
+++ The_Believer's_Quest/Assets/Scripts/Entities/MovingObject.cs
@@ -7,14 +7,14 @@ public class MovingObject : MonoBehaviour
     {
         bool noCollision = true;
         //RaycastHit2D hit1 = Physics2D.Raycast(pos + new Vector2(0.2f, 0.01f), new Vector2(x, y), 0.03f);
-        RaycastHit2D hit2 = Physics2D.Raycast(pos + new Vector2(0.2f, -0.4f), new Vector2(x, y), 0.03f);
+        RaycastHit2D hit2 = Physics2D.Raycast(pos + new Vector2(0.2f, -0.4f), new Vector2(x, y), 0.06f);
         //RaycastHit2D hit3 = Physics2D.Raycast(pos + new Vector2(-0.2f, 0.01f), new Vector2(x, y), 0.03f);
-        RaycastHit2D hit4 = Physics2D.Raycast(pos + new Vector2(-0.2f, -0.4f), new Vector2(x, y), 0.03f);
+        RaycastHit2D hit4 = Physics2D.Raycast(pos + new Vector2(-0.2f, -0.4f), new Vector2(x, y), 0.06f);
 
         //Debug.DrawRay(pos + new Vector2(0.2f, 0.01f), new Vector2(x, y) * 0.03f, Color.red);
-        Debug.DrawRay(pos + new Vector2(0.2f, -0.4f), new Vector2(x, y) * 0.03f, Color.red);
+        Debug.DrawRay(pos + new Vector2(0.2f, -0.4f), new Vector2(x, y) * 0.06f, Color.red);
         //Debug.DrawRay(pos + new Vector2(-0.2f, 0.01f), new Vector2(x, y) * 0.03f, Color.red);
-        Debug.DrawRay(pos + new Vector2(-0.2f, -0.4f), new Vector2(x, y) * 0.03f, Color.red);
+        Debug.DrawRay(pos + new Vector2(-0.2f, -0.4f), new Vector2(x, y) * 0.06f, Color.red);
 
         if (/*hit1 ||*/ hit2 || /*hit3 ||*/ hit4) //origine, direction, taille)
         {
