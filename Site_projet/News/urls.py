cd5b856510f236bc77c53ee60ae211429e40df53
Unikarah <sarah.gutierez@epita.fr>
2019-04-02 23:08:49 +0200

    backup?

--- Site_projet/News/urls.py
+++ Site_projet/News/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.views.generic import ListView, DetailView
-from News.models import Article
+from News.models import Article, Snippet
 
 
 urlpatterns = [
