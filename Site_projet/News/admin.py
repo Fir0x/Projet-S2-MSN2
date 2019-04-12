7842524478b503311090afe9bf9c1d87173180fb
Nicolas INDJEIN <nicolas.indjein@epita.fr>
2019-04-12 23:31:40 +0200

    upload du site -> transfert des fichiers du site vers git d'Heroku

--- Site_projet/News/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from News.models import Article, Snippet
-
-# Register your models here.
-admin.site.register(Article)
-admin.site.register(Snippet)
--- Site_projet/News/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Snippet
-
-class SnippetForm(forms.ModelForm):
-
-    class Meta:
-        model = Snippet
-        fields = ('name', 'email', 'message')
