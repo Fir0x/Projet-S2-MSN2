4e873b5dea185edb2437f1e3c88451fee62370a3
Sarah G <sarah.gutierez@epita.fr>
2019-04-16 00:20:05 +0200

    menu pause sans intéractions

---
+++ The_Believer's_Quest/Assets/Scripts/UI/Menupause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menupause : MonoBehaviour
+{
+    public static bool show = false;
+    public GameObject canvaspause;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (show)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    void Resume()
+    {
+        canvaspause.SetActive(false);
+        Time.timeScale = 1f;
+        show = false;
+    }
+
+    void Pause()
+    {
+        canvaspause.SetActive(true);
+        Time.timeScale = 0f;
+        show = true;
+    }
+}
