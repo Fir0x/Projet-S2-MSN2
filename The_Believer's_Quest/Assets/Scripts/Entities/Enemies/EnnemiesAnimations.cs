f12fd53a88b2b6563e191b7baba58d84d3d5e132
Nicolas INDJEIN <nicolas.indjein@epita.fr>
2019-04-19 18:29:22 +0200

    script animation générale pour ennemis fait

---
+++ The_Believer's_Quest/Assets/Scripts/Entities/Enemies/EnnemiesAnimations.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnnemiesAnimations : MonoBehaviour
+{
+    [SerializeField] protected AnimationClip[] animationClips;
+    private Animator animator;
+    private AnimatorOverrideController animatorOverrideController;
+
+    public void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animatorOverrideController["Move_Up"] = animationClips[0];
+        animatorOverrideController["Move_Right"] = animationClips[1];
+        animatorOverrideController["Move_Low"] = animationClips[2];
+        animatorOverrideController["Move_Left"] = animationClips[3];
+        animatorOverrideController["Attack"] = animationClips[4];
+        animatorOverrideController["Death"] = animationClips[5];
+
+        animator.runtimeAnimatorController = animatorOverrideController;
+    }
+}
