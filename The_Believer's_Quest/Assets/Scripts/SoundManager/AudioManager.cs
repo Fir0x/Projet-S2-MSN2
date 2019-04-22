dfbf6fd92f1e74d860cb5bdbc82b983a849dd27f
Maxence PLANTARD <maxence.plantard@epita.fr>
2019-04-22 20:53:50 +0200

    men blc

---
+++ The_Believer's_Quest/Assets/Scripts/SoundManager/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource BO;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeBO(AudioClip music)
+    {
+        if (BO.clip.name == music.name) return;
+        
+        BO.Stop();
+        BO.clip = music;
+        BO.Play();
+    }
+}
---
+++ The_Believer's_Quest/Assets/Scripts/SoundManager/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+        public AudioSource efxSource;                   
+        public AudioSource musicSource;                 
+        public static SoundManager instance = null;      
+        
+        
+        void Awake ()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy (gameObject);
+            DontDestroyOnLoad (gameObject);
+        }
+        
+        public void PlaySingle(AudioClip clip)
+        {
+            efxSource.clip = clip;
+            efxSource.Play ();
+        }
+        
+        public void RandomizeSfx (params AudioClip[] clips)
+        {
+            int randomIndex = Random.Range(0, clips.Length);
+            efxSource.clip = clips[randomIndex];
+            efxSource.Play();
+        }
+}
+    
