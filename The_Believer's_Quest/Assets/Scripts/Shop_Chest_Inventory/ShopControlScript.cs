a2d0f5dcd33270b5f93e17e6ebe62bf6dc4dfeee
Maxence PLANTARD <maxence.plantard@epita.fr>
2019-04-23 04:27:20 +0200

    fr

---
+++ The_Believer's_Quest/Assets/Scripts/Shop_Chest_Inventory/ShopControlScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    public Text moneyAmountText;
+    public Text itemPrice;
+    public Text areaText;
+    [SerializeField] private PlayerAsset playerasset;
+    private int GoldAmountP;
+    private int DiamondAmountP;
+
+    public PlayerAsset Playerasset
+    {
+        get  => playerasset; set => playerasset = value; 
+    }
+
+    void Start()
+    {
+        GoldAmountP = playerasset.Gold;
+        DiamondAmountP = playerasset.Diamond;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
