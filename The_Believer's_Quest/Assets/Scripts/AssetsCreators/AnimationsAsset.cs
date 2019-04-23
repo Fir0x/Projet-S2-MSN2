e81595b7878bac042638f215aa0bf1cc90044a32
Nicolas INDJEIN <nicolas.indjein@epita.fr>
2019-04-23 12:54:47 +0200

    test fix

---
+++ The_Believer's_Quest/Assets/Scripts/AssetsCreators/AnimationsAsset.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewAnimationsAsset", menuName = "Entity/Animations!mm")]
+public class AnimationsAsset : ScriptableObject
+{
+    [SerializeField] private Animation[] animations;
+
+    public Animation[] Animations { get => animations; set => animations = value; }
+}
