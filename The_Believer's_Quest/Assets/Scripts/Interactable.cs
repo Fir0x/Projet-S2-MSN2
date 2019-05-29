03d04dcd25fceaa02b06598eba632dd2c451d488
Maxence PLANTARD <maxence.plantard@epita.fr>
2019-05-30 01:55:29 +0200

    issou la chancla

---
+++ The_Believer's_Quest/Assets/Scripts/Interactable.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;               
+    public Transform interactionTransform;  
+
+    bool isFocus = false;   
+    Transform player; 
+
+    bool isOpen = false; 
+
+    public virtual void Interact() {}
+
+    void Update()
+    {
+        if (isFocus && !isOpen)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                isOpen = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        isOpen = false;
+    }
+    
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        isOpen = false;
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+        if (interactionTransform == null)
+            interactionTransform = transform;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+
+}
