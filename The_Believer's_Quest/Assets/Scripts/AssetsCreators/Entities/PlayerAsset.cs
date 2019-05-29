f590f5f6db8ee653370d186d8cb5c004725de64e
TheNikow <nicolas.lorrain@epita.fr>
2019-05-29 11:27:39 +0200

    bug attack

--- The_Believer's_Quest/Assets/Scripts/AssetsCreators/Entities/PlayerAsset.cs
+++ The_Believer's_Quest/Assets/Scripts/AssetsCreators/Entities/PlayerAsset.cs
@@ -14,6 +14,7 @@ public class PlayerAsset : ScriptableObject
     [SerializeField] private int gold;
     [SerializeField] private int diamond;
     [SerializeField] private WeaponAsset[] weaponsList = new WeaponAsset[2];
+    [SerializeField] private Vector2 position;
 
     public int Floor { get => floor; set => floor = value; }
     public int Hp { get => hp; set => hp = value; }
@@ -25,6 +26,7 @@ public class PlayerAsset : ScriptableObject
     public int Gold { get => gold; set => gold = value; }
     public int Diamond { get => diamond; set => diamond = value; }
     public WeaponAsset[] WeaponsList { get => weaponsList; set => weaponsList = value; }
+    public Vector2 Position { get => position; set => position = value; }
 
     public string SerializeWeapons()
     {
