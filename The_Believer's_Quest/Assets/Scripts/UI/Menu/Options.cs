2d42502b8f312bff8c030c54e79477f588ac97fe
Nicolas INDJEIN <nicolas.indjein@epita.fr>
2019-05-30 17:48:24 +0200

    sauvegarde param son finie

--- The_Believer's_Quest/Assets/Scripts/UI/Menu/Options.cs
+++ The_Believer's_Quest/Assets/Scripts/UI/Menu/Options.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class Options : MonoBehaviour
 {
     //Sarah
-    public AudioMixer audioMixer;
-    
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider soundSlider;
+    [SerializeField] private Slider musicSlider;
+
+    public AudioMixer AudioMixer { get => audioMixer; set => audioMixer = value; }
+    public Slider SoundSlider { get => soundSlider; set => soundSlider = value; }
+    public Slider MusicSlider { get => musicSlider; set => musicSlider = value; }
+
+    private void Start() //Nicolas I
+    {
+        if (File.Exists("playerSettings.bin"))
+        {
+            Tuple<float,float> soundInfos =  Loader.LoadingPlayerSettings();
+            VolumeEffect(soundInfos.Item1);
+            VolumeMusic(soundInfos.Item2);
+            soundSlider.value = soundInfos.Item1;
+            musicSlider.value = soundInfos.Item2;
+        }
+        else
+        {
+            float BGS;
+            float BGM;
+            audioMixer.GetFloat("Sound", out BGS);
+            audioMixer.GetFloat("Music", out BGM);
+            Saver.SavePlayerSettings(BGS, BGM);
+        }
+    }
+
     public void VolumeMusic(float volume)
     {
-        audioMixer.SetFloat("Music", volume);
+        AudioMixer.SetFloat("Music", volume);
         GameObject.FindGameObjectWithTag("SoundManager").GetComponent<SoundManager>().ChangeVolume(volume);
     }
     public void VolumeEffect(float volume)
@@ -18,5 +45,12 @@ public class Options : MonoBehaviour
         audioMixer.SetFloat("Sound", volume);
     }
 
-
+    public void SaveChange()
+    {
+        float BGS;
+        float BGM;
+        audioMixer.GetFloat("Sound", out BGS);
+        audioMixer.GetFloat("Music", out BGM);
+        Saver.SavePlayerSettings(BGM, BGS);
+    }
 }
