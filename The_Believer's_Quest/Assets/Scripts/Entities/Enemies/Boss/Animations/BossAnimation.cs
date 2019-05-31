2edd3f71234babb3cbbaf774dfd8181b104ac9d9
Nicolas INDJEIN <nicolas.indjein@epita.fr>
2019-06-01 01:20:43 +0200

    boss animator

---
+++ The_Believer's_Quest/Assets/Scripts/Entities/Enemies/Boss/Animations/BossAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimation : MonoBehaviour
+{
+    [SerializeField] private AnimationClip[] move;
+    [SerializeField] private AnimationClip[] attack;
+    [SerializeField] private AnimationClip changePhase;
+    [SerializeField] private AnimationClip death;
+
+    private Animator animator;
+    private AnimatorOverrideController animatorOverrideController;
+
+    private int animMoveID = Animator.StringToHash("direction");
+    private int animPhaseTwoID = Animator.StringToHash("phase2");
+
+    public AnimationClip[] Move { get => move; set => move = value; }
+    public AnimationClip[] Attack { get => attack; set => attack = value; }
+    public AnimationClip ChangePhase { get => changePhase; set => changePhase = value; }
+    public AnimationClip Death { get => death; set => death = value; }
+
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animatorOverrideController["Move"] = move[0];
+        animatorOverrideController["Attack"] = attack[0];
+        animatorOverrideController["ChangePhase"] = changePhase;
+        animatorOverrideController["Death"] = death;
+
+        animator.runtimeAnimatorController = animatorOverrideController;
+    }
+
+    public void ChangeMove()
+    {
+        animatorOverrideController["Move"] = move[animator.GetInteger(animMoveID) + 4 * (animator.GetBool(animPhaseTwoID) ? 1 : 0)];
+    }
+
+    public void ChangeAttack()
+    {
+        animatorOverrideController["Attack"] = attack[animator.GetInteger(animMoveID) + 4 * (animator.GetBool(animPhaseTwoID) ? 1 : 0)];
+    }
+}
---
+++ The_Believer's_Quest/Assets/Scripts/Entities/Enemies/Normals/Animations/EnemyAnimation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+//Nicolas I
+public class EnemyAnimation : MonoBehaviour
+{
+    [SerializeField] private AnimationClip[] move;
+    [SerializeField] private AnimationClip[] attack;
+    [SerializeField] private AnimationClip death;
+
+    private Animator animator;
+    private AnimatorOverrideController animatorOverrideController;
+
+    private int animMoveID = Animator.StringToHash("direction");
+
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        /*animatorOverrideController["Move_Up"] = animationClips[0];
+        animatorOverrideController["Move_Right"] = animationClips[1];
+        animatorOverrideController["Move_Low"] = animationClips[2];
+        animatorOverrideController["Move_Left"] = animationClips[3];
+        animatorOverrideController["Attack"] = animationClips[4];
+        animatorOverrideController["Death"] = animationClips[5];*/
+
+        animatorOverrideController["Move"] = move[0];
+        animatorOverrideController["Attack"] = attack[0];
+        animatorOverrideController["Death"] = death;
+
+        animator.runtimeAnimatorController = animatorOverrideController;
+    }
+
+    public void ChangeMove()
+    {
+        animatorOverrideController["Move"] = move[animator.GetInteger(animMoveID)];
+    }
+
+    public void ChangeAttack()
+    {
+        animatorOverrideController["Attack"] = attack[animator.GetInteger(animMoveID)];
+    }
+}
