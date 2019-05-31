f1de32348b21de1e80a58f81e1ad473ec7a85ef0
Maxence PLANTARD <maxence.plantard@epita.fr>
2019-05-31 02:57:16 +0200

    ma bite le chest fini

---
+++ The_Believer's_Quest/Assets/Scripts/UI/Chest/ChestUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class ChestUI : MonoBehaviour
+{
+    public Transform itemsParent;
+    public GameObject chestUI;
+
+    Chest chest;
+
+    ChestSlot[] slots;
+
+    void Start()
+    {
+        chest = Chest.instance;
+        chest.onItemChangedCallback += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<ChestSlot>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            chestUI.SetActive(!chestUI.activeSelf);
+        }
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < chest.items.Count)
+            {
+                slots[i].AddItem(chest.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
