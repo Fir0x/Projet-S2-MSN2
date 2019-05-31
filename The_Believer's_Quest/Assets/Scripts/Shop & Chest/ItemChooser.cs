05c364f8fcfc5994b8ba71b6b6364d76eb6d4f78
Maxence PLANTARD <maxence.plantard@epita.fr>
2019-05-31 17:01:34 +0200

    aya

--- The_Believer's_Quest/Assets/Scripts/Shop & Chest/ItemChooser.cs
+++ The_Believer's_Quest/Assets/Scripts/Shop & Chest/ItemChooser.cs
@@ -31,29 +31,5 @@ public class ItemChooser : MonoBehaviour
         return items;
     }
 
-    public List<GameObject> ChooseContent(UnlockedItemsAsset availableItems)
-    {
-        int nbItems = Random.Range(1, 5);
-        List<GameObject> items = new List<GameObject>();
-
-        float minRarity = Random.Range(0.2f, ((10 / 7) - 0.19f) / 2);
-        float maxRarity = Random.Range(((10 / 7) - 0.2f) / 2, 10 / 7);
-        List<GameObject> choosen = (from item in availableItems.Unlocked
-                                    where (item.GetComponent<Weapon>() != null &&
-                                          100 / item.GetComponent<Weapon>().GetAsset().Price >= minRarity &&
-                                          100 / item.GetComponent<Weapon>().GetAsset().Price <= maxRarity) ||
-                                          (item.GetComponent<Object>() != null &&
-                                          100 / item.GetComponent<Object>().GetAsset().Price >= minRarity &&
-                                          100 / item.GetComponent<Object>().GetAsset().Price <= maxRarity)
-                                    select item).ToList();
-
-        for (int i = 0; i < nbItems; i++)
-        {
-            int itemIndex = Random.Range(0, choosen.Count);
-            items[i] = choosen[itemIndex];
-            choosen.RemoveAt(itemIndex);
-        }
-
-        return items;
-    }
+    
 }
