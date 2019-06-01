2d741f95b47816cc425d4e680a4461078611fc66
TheNikow <nicolas.lorrain@epita.fr>
2019-06-01 02:11:06 +0200

    chest;;

--- The_Believer's_Quest/Assets/Scripts/UI/Inventory/InventorySlot.cs
+++ The_Believer's_Quest/Assets/Scripts/UI/Inventory/InventorySlot.cs
@@ -8,10 +8,10 @@ public class InventorySlot : MonoBehaviour
 
     public Image icon;          // Reference to the Icon image
 
-    ObjectsAsset item;  // Current item in the slot
+    GameObject item;  // Current item in the slot
 
     // Add item to the slot
-    public void AddItem(ObjectsAsset newItem)
+    public void AddItem(GameObject newItem)
     {
         item = newItem;
         icon.sprite = item.Sprite;
