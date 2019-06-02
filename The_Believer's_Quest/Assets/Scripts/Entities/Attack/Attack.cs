466605f860603d13d172096091b333f299104ad3
TheNikow <nicolas.lorrain@epita.fr>
2019-06-02 23:37:39 +0200

    oui

--- The_Believer's_Quest/Assets/Scripts/Entities/Attack/Attack.cs
+++ The_Believer's_Quest/Assets/Scripts/Entities/Attack/Attack.cs
@@ -24,9 +24,9 @@ public class Attack : MonoBehaviour
         if (traj == Trajectory.Line)
             LineShot(playerT.position, enemy.Speed, boss.Damage, 0f);
         if (traj == Trajectory.Arc)
-            ArcShot(enemy.NbOfProjectiles, playerT.position, enemy.ShotSpeed, boss.Damage);
+            ArcShot(boss.NbOfProjectiles, playerT.position, boss.ShotSpeed, boss.Damage);
         if (traj == Trajectory.Circle)
-            CircleShot(enemy.NbOfProjectiles, enemy.ShotSpeed, boss.Damage);
+            CircleShot(boss.NbOfProjectiles, boss.ShotSpeed, boss.Damage);
         if (traj == Trajectory.Cqc)
             Cqc(boss.Damage);
         if (traj == Trajectory.Railgun)
